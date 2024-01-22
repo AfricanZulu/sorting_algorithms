cf39711892558f21c034b2e65385dd3b81fcb21a
AfricanZulu <NomaAfrikagracezulu@gmail.com>
2024-01-22 11:37:09 -0800

    sorting algorithms

---
+++ 102-counting_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * get_max - Get the maximum value in an array of integer.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Return: The maximum integer in the array.
+ */
+int get_max(int *array, int size)
+{
+	int max, i;
+
+	for (max = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
+}
+
+/**
+ * counting_sort - Sort an array of integers in ascending order
+ *                 using the counting sort algorithm.
+ * @array: An array of integer.
+ * @size: The size of the array.
+ *
+ * Description: Prints the counting array after setting it up.
+ */
+void counting_sort(int *array, size_t size)
+{
+	int *count, *sorted, max, i;
+
+	if (array == NULL || size < 2)
+		return;
